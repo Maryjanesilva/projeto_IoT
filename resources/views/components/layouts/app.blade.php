d1f6ab8ee0c408ed696d85dac52e95cd758f3a3a
Maryjanesilva <ribeirodasilvam083@gmail.com>
2025-09-30 15:29:33 -0300

    registro

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,3 +1,7 @@
+
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -64,3 +68,4 @@
 </body>
 
 </html>
+
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\RegistroController;
+
 use App\Livewire\Ambiente\AmbienteCreate;
 use App\Livewire\Ambiente\AmbienteEdit;
 use App\Livewire\Ambiente\AmbienteList;
@@ -8,6 +10,9 @@ use App\Livewire\Sensor\Sensoredit;
 use App\Livewire\Sensor\Sensorlist;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/ambiente/create',AmbienteCreate::class)->name('ambiente.create');
+Route::get('/ambiente/list',AmbienteList::class)->name('ambiente.list');
+Route::get('/ambiente/edit/{id}',AmbienteEdit::class)->name('ambiente.edit');
 
 Route::get('/sensor/create',SensorCreate::class)->name('sensor.create');
 Route::get('/sensor/list',Sensorlist::class)->name('sensor.list');
