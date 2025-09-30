11c81dad7501f99f115a1b719d95df50e8fafec1
Dona Mary <162075803+Maryjanesilva@users.noreply.github.com>
2025-09-30 15:44:16 -0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,4 @@ use App\Http\Controllers\RegistroController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
 Route::post('registro/index', [RegistroController::class, 'store']);
